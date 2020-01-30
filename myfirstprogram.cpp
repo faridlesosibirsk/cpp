aaf3e2f2fe1520131d898db7efe3be15588e572c
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-30 08:49:45 +0700

    Variables

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,12 +2,9 @@
 using namespace std;
 
 int main() {
-    int myNum = 5; // Integer (whole number without decimals)
-    double myFloatNum = 5.99; // Floating point number (with decimals)
-    char myLetter = 'D'; // Character
-    string myText = "Hello"; // String (text)
-    bool myBoolean = true; // Boolean (true or false)
-    int myAge = 35;
-    cout << "I am " << myAge << " years old." << endl;
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    cout << sum << endl;
     return 0;
 }
