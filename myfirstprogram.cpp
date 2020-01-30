3e8e5814276a6f07e9bc72551aa8dd2a3735bd3c
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-30 08:43:26 +0700

    Variables

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    int myNum;
-    myNum = 15;
-    cout << myNum << endl;
+    int myNum = 15; // myNum is 15
+    myNum = 10; // Now myNum is 10
+    cout << myNum << endl; // Outputs 10
     return 0;
 }
