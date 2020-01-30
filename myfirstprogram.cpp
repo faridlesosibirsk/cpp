733cec8f3b577985f49688afa574c465eef332c2
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-30 08:48:36 +0700

    Variables

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main() {
-    const int myNum = 15; // myNum will always be 15
-    myNum = 10; // error: assignment of read-only variable 'myNum'
-    cout << myNum << endl; // Outputs 10
+    int myNum = 5; // Integer (whole number without decimals)
+    double myFloatNum = 5.99; // Floating point number (with decimals)
+    char myLetter = 'D'; // Character
+    string myText = "Hello"; // String (text)
+    bool myBoolean = true; // Boolean (true or false)
+    int myAge = 35;
+    cout << "I am " << myAge << " years old." << endl;
     return 0;
 }
