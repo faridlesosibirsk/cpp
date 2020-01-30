50bb64337c61d5753a0254b588c6c02b6bc850dc
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-30 08:53:55 +0700

    User Input

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    int x =5, y = 6, z = 50;
-    cout << x + y + z << endl;
+    int x =5;
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
+    cout << "Your number is: " << x << endl; // Display the input value
     return 0;
 }
