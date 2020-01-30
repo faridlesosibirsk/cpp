6c7d8b78454356cf604b228e421d8ff6277d4e7e
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-30 08:38:21 +0700

    Comments

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  // This is a comment        
+  cout << "Hello World!" << endl; // This is a comment
+  /* The code below will print the words Hello World!
+  to the screen, and it is amazing */
   cout << "I am learnig C++\n";
   return 0;
 }
