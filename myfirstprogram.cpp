6eff41fc0b9f606c1613b4e47ea7038aebbadc6b
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:20:13 +0700

    String Data Types

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main() {
     // Creating variables
-    char myGrade = 'B';
-    char a = 65, b = 66, c = 67;
-    cout << myGrade << endl;
-    cout << a;
-    cout << b;
-    cout << c << endl;
+    string greeting = "Hello";
+    cout << greeting << endl;
 
     return 0;
 }
