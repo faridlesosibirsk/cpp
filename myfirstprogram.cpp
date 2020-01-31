5a548a2422b855d2e06e0f5295b0bf3e15539fcf
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:37:21 +0700

    Logical Operators

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -4,12 +4,9 @@ using namespace std;
 int main() {
     int x = 10;
     int y = 3;
-    cout << (x == y) << endl;
-    cout << (x != y) << endl;
-    cout << (x > y) << endl;
-    cout << (x < y) << endl;
-    cout << (x >= y) << endl;
-    cout << (x <= y) << endl;
+    cout << (x < 5 && x < 10) << endl;
+    cout << (x < 5 || x < 4) << endl;
+    cout << !(x < 5 && x < 10) << endl;
 
     return 0;
 }
