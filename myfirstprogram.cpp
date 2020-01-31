98105f3cc7bcc069b80787779e6ec8ec9d5e8547
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:40:31 +0700

    String Concatenation

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main() {
-    int x = 10;
-    int y = 3;
-    cout << (x < 5 && x < 10) << endl;
-    cout << (x < 5 || x < 4) << endl;
-    cout << !(x < 5 && x < 10) << endl;
+    string firstName = "John";
+    string lastName = "Doe";
+    string fullName = firstName + lastName;
+    cout << fullName << endl;
 
     return 0;
 }
