a04e17092c4934fad294c9c88a220ca66080de65
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 22:06:28 +0700

    Pointers

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 
 int main() {
-    string food = "Pizza";  // Variable declaration
-    string* ptr = &food;    // Pointer declaration
+    string food = "Pizza";
+    string* ptr = &food;
 
-    // Reference: Output the memory address of food with the pointer (0x6dfed4)
-    cout << ptr << "\n";
+    // Output the value of food (Pizza)
+    cout << food << "\n";
 
-    // Dereference: Output the value of food with the pointer (Pizza)
+    // Output the memory address of food (0x6dfed4)
+    cout << &food << "\n";
+
+    // Access the memory address of food and output its value (Pizza)
     cout << *ptr << "\n";
+
+    // Change the value of the pointer
+    *ptr = "Hamburger";
+
+    // Output the new value of the pointer (Hamburger)
+    cout << *ptr << "\n";
+
+    // Output the new value of the food variable (Hamburger)
+    cout << food << "\n";
 
     return 0;
 }
