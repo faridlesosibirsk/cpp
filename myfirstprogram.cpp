20ca197d916f40f8ae7044c27cd4d786793ad427
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:08:30 +0700

    Numeric Data Types

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
     // Creating variables
-    int myNum = 5; // Integer (whole number)
-    float myFloatNum = 5.99; // Floating point number
-    double myDoubleNum = 9.98; // Floating point number
-    char myLetter = 'D'; // Character
-    bool myBoolean = true; // Boolean
-    string myString = "Hello"; // String
-
-    // Print variable values
-    cout << "int: " << myNum << "\n";
-    cout << "float: " << myFloatNum << endl;
-    cout << "double: " << myDoubleNum << endl;
-    cout << "char: " << myLetter << endl;
-    cout << "bool: " << myBoolean << endl;
-    cout << "string: " << myString << endl;
+    int myNum = 1000;
+    cout << myNum << endl;
 
     return 0;
 }
