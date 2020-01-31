080f98e2167766022d38ad4491d7ad98d0e881bb
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:58:25 +0700

    References

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::string firstName = "John";
-    std::string lastName = "Doe";
-    std::string fullName = firstName + lastName;
-    std::cout << fullName << "\n";
+    string food = "Pizza";  // food variable
+    string &meal = food;    // reference to food
+    string f2;
+    f2 = food;
+    food = "test";
+    f2 = "food2";
+    cout << food << "\n";  // Outputs Pizza
+    cout << meal << "\n";  // Outputs Pizza
+    cout << f2 << "\n";
 
     return 0;
 }
