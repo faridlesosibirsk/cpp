2bb4c85c16ef16968e2dfdeded3ac08e666b73d6
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:26:35 +0700

    Operators

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 
 int main() {
-    int sum1 = 100 + 50;
-    int sum2 = sum1 + 250;
-    int sum3 = sum2 + sum2;
-    cout << sum3 << endl;
+    int x = 51;
+    int y = 2;
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << x / y << endl;
+    cout << ++x << endl;
+    --x;
+    cout << --x << endl;
 
     return 0;
 }
