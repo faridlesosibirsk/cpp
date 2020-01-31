4ee4fe489219e1bb00387660d9b73977e1beaf0e
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 22:09:25 +0700

    Functions

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string food = "Pizza";
-    string* ptr = &food;
-
-    // Output the value of food (Pizza)
-    cout << food << "\n";
-
-    // Output the memory address of food (0x6dfed4)
-    cout << &food << "\n";
-
-    // Access the memory address of food and output its value (Pizza)
-    cout << *ptr << "\n";
-
-    // Change the value of the pointer
-    *ptr = "Hamburger";
-
-    // Output the new value of the pointer (Hamburger)
-    cout << *ptr << "\n";
-
-    // Output the new value of the food variable (Hamburger)
-    cout << food << "\n";
-
-    return 0;
+// Create a function
+void myFunction() {
+  cout << "I just got executed!" << endl;
 }
+
+int main() {
+  myFunction(); // call the function
+  myFunction();
+  myFunction();
+  return 0;
+}
+
+// Outputs "I just got executed!"
