74e28f8d5450793e46f5cf12141fae404e71a871
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 22:03:40 +0700

    Dereference

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 
 int main() {
-    string food = "Pizza";  // A food variable of type string
-    string* ptr = &food;    // A pointer variable, with the name ptr, that stores the address of food
+    string food = "Pizza";  // Variable declaration
+    string* ptr = &food;    // Pointer declaration
 
-    // Output the value of food (Pizza)
-    cout << food << "\n";
-
-    // Output the memory address of food (0x6dfed4)
-    cout << &food << "\n";
-
-    // Output the memory address of food with the pointer (0x6dfed4)
+    // Reference: Output the memory address of food with the pointer (0x6dfed4)
     cout << ptr << "\n";
+
+    // Dereference: Output the value of food with the pointer (Pizza)
+    cout << *ptr << "\n";
 
     return 0;
 }
