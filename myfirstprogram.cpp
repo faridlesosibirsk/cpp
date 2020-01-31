00b12227383b2f35adf5196bdb31810577270a89
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:06:17 +0700

    Data Types

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    int x, y;
-    int sum;
-    cout << "Type a number: ";
-    cin >> x;
-    cout << "Type another number: ";
-    cin >> y;
-    sum = x + y;
-    cout << "Sum is: " << sum << endl;
+    // Creating variables
+    int myNum = 5; // Integer (whole number)
+    float myFloatNum = 5.99; // Floating point number
+    double myDoubleNum = 9.98; // Floating point number
+    char myLetter = 'D'; // Character
+    bool myBoolean = true; // Boolean
+    string myString = "Hello"; // String
+
+    // Print variable values
+    cout << "int: " << myNum << "\n";
+    cout << "float: " << myFloatNum << endl;
+    cout << "double: " << myDoubleNum << endl;
+    cout << "char: " << myLetter << endl;
+    cout << "bool: " << myBoolean << endl;
+    cout << "string: " << myString << endl;
+
     return 0;
 }
