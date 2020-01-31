143413606066f11fa9a7b8f4f836e5670372245a
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:59:40 +0700

    Memory Address

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -3,14 +3,7 @@ using namespace std;
 
 int main() {
     string food = "Pizza";  // food variable
-    string &meal = food;    // reference to food
-    string f2;
-    f2 = food;
-    food = "test";
-    f2 = "food2";
-    cout << food << "\n";  // Outputs Pizza
-    cout << meal << "\n";  // Outputs Pizza
-    cout << f2 << "\n";
+    cout << &food << "\n";  // Outputs Pizza
 
     return 0;
 }
