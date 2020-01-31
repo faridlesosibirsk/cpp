176a217617c38cb6ebc48219fec2bf61f16a4190
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2020-01-31 21:11:29 +0700

    Numeric Data Types

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main() {
     // Creating variables
-    double myNum = 1000.75;
-    cout << myNum << endl;
+    double d1 = 35e3;
+    float f1 = 12E4;
+    cout << d1 << endl;
+    cout << f1 << endl;
 
     return 0;
 }
